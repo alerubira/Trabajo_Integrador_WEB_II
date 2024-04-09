c7f34d6b7a98e4cad88cf9fa902a35c068c8889a
alerubira <100098255+alerubira@users.noreply.github.com>
2024-04-08 21:02:36 -0300

    ajustar cartel de oferta

--- procesar.js
+++ procesar.js
@@ -1,22 +1,26 @@
 const traductor=require('node-google-translate-skidz');
 let arregloConOfertas=[];
 let productos=[];
-function recibir(arreglo){
-    arregloConOfertas=arreglo;
-}
-function procesarProductos(listaP){
 
-    
-    console.log(arregloConOfertas);
+function procesarProductos(listaP){
+  
     listaP.forEach((element) => {
-      
-        //imagen del producto, título, descripción (máximo 30 caracteres), categoría y precio. 
+      let ofertaEncontrada = arregloConOfertas.find(oferta => oferta.id === element.id);
+        //el precio origina, el descuento, el monto descontado y el precio final.  
         let producto={};
+        if(ofertaEncontrada){
+          producto.precio=element.price;
+          producto.descuento=ofertaEncontrada.porcentaje;
+          producto.dineroDescontado=dineroDescontados(producto.precio,producto.descuento);
+          producto.precioFinal=producto.precio-producto.dineroDescontado;
+        }else{
+          producto.precio=element.price;
+        }
           producto.id=element.id;
           producto.imagen=element.image;
           producto.titulo= element.title;
           producto.categoria=element.category;
-          producto.precio=element.price;
+          
           producto.descripcion=element.description;
           productos.push(producto);
         
@@ -25,7 +29,14 @@ function procesarProductos(listaP){
       });
       return productos;
 }
+function recibir(arreglo){
+    arregloConOfertas=arreglo;
+}
 module.exports={procesarProductos,recibir};
+function dineroDescontados(precio,descuento){
+
+return (precio*descuento)/100;
+}
 function traducir(texto){
     let traducido;
     traductor({
@@ -35,9 +46,6 @@ function traducir(texto){
     }, function(result) {
       traducido= result.translation;
       return traducido;
-      console.log(traducido);
-     
-     
-    });
-    return traducido;
+      });
+    
   }
